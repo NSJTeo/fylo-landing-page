aa69f4bd340ff5de4c41e85e8fff2f5e2ceb1655
nsjteo <n.teomacdonald@gmail.com>
2022-06-07 14:14:03 -0700

    Info component desktop styles

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -5,22 +5,24 @@
       src="../assets/illustrations/illustration-stay-productive.png"
       alt=""
     />
-    <p className="info__header">Stay productive, wherever you are</p>
-    <p className="info__text">
-      Never let location be an issue when accessing your files. Fylo has you
-      covered for all of your file storage needs.
-    </p>
-    <p className="info__text">
-      Securely share files and folders with friends, family and colleagues for
-      live collaboration. No email attachments required.
-    </p>
-    <div className="info__link">
-      <p className="info__link-text">See how Fylo works</p>
-      <img
-        className="info__link-image"
-        src="../assets/icons/icon-arrow.svg"
-        alt=""
-      />
+    <div>
+      <p className="info__header">Stay productive, wherever you are</p>
+      <p className="info__text">
+        Never let location be an issue when accessing your files. Fylo has you
+        covered for all of your file storage needs.
+      </p>
+      <p className="info__text">
+        Securely share files and folders with friends, family and colleagues for
+        live collaboration. No email attachments required.
+      </p>
+      <div className="info__link">
+        <p className="info__link-text">See how Fylo works</p>
+        <img
+          className="info__link-image"
+          src="../assets/icons/icon-arrow.svg"
+          alt=""
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -32,15 +34,31 @@ export default {
 </script>
 
 <style lang="scss">
+@use '../styles/mixins.scss' as *;
+
 .info {
   &__container {
     max-width: 1440px;
     padding: 0 28px;
     padding-bottom: 160px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    @include info {
+      flex-direction: row;
+      padding: 0 101px 120px 105px;
+    }
   }
   &__image {
     margin-bottom: 48px;
     width: 100%;
+    max-width: 615px;
+    @include info {
+      margin: 0;
+      margin-right: 57px;
+      width: 50%;
+    }
   }
   &__header {
     margin-bottom: 1rem;
@@ -51,6 +69,13 @@ export default {
     line-height: 24px;
     text-align: center;
     color: #ffffff;
+    @include info {
+      font-size: 40px;
+      line-height: 50px;
+      margin-bottom: 19px;
+      text-align: start;
+      max-width: 478px;
+    }
   }
   &__text {
     margin-bottom: 1rem;
@@ -60,6 +85,11 @@ export default {
     font-size: 14px;
     line-height: 21px;
     color: #ffffff;
+    max-width: 563px;
+    @include info {
+      font-size: 16px;
+      line-height: 24px;
+    }
   }
   &__link {
     padding-bottom: 6px;
@@ -67,6 +97,9 @@ export default {
     width: fit-content;
     display: flex;
     align-items: center;
+    @include info {
+      margin-top: 24px;
+    }
   }
   &__link-text {
     margin-right: 7px;
