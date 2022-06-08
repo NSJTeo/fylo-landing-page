ad6f773832d5f4a4f5010cebaf63a3742e8117c0
nsjteo <n.teomacdonald@gmail.com>
2022-06-08 15:38:44 -0700

    Deployment file and config

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nsjteo/fylo-landing-page.git main:gh-pages
+
+cd -
